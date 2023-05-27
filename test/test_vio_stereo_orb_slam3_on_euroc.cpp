d717ba6d080523dfdf3b8977ba6487ac78ef614e
Horizon1026 <lijiapeng_atlantis@163.com>
2023-05-27 15:17:33 +0800

    add code

---
+++ test/test_vio_stereo_orb_slam3_on_euroc.cpp
@@ -0,0 +1,12 @@
+#include "frontend.h"
+#include "backend.h"
+#include "data_loader.h"
+#include "data_manager.h"
+
+#include "log_report.h"
+
+int main(int argc, char **argv) {
+    ReportInfo(YELLOW ">> Test vio stereo orb slam3 on euroc dataset." RESET_COLOR);
+
+    return 0;
+}
