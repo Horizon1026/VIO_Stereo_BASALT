19c2c4476fa7d7166ad2d63c401af51bd6fcfb23
Horizon1026 <lijiapeng_atlantis@163.com>
2023-05-30 00:11:11 +0800

    vio stereo orb slam3

--- src/data_loader/data_loader.h
+++ src/data_loader/data_loader.h
@@ -1,4 +1,26 @@
 #ifndef _VIO_STEREO_ORB_SLAM3_DATA_LOADER_H_
 #define _VIO_STEREO_ORB_SLAM3_DATA_LOADER_H_
 
+#include "datatype_basic.h"
+#include "datatype_image.h"
+#include "datatype_image_pyramid.h"
+
+#include "imu_state.h"
+
+namespace VIO {
+
+/* Class Data Loader Declaration. */
+class DataLoader final {
+
+public:
+    DataLoader() = default;
+    ~DataLoader() = default;
+
+private:
+    std::list<SENSOR_MODEL::ImuMeasurement> imu_buffer_;
+
+};
+
+}
+
 #endif // end of _VIO_STEREO_ORB_SLAM3_DATA_LOADER_H_
--- src/data_manager/data_manager.h
+++ src/data_manager/data_manager.h
@@ -1,4 +1,31 @@
 #ifndef _VIO_STEREO_ORB_SLAM3_DATA_MANAGER_H_
 #define _VIO_STEREO_ORB_SLAM3_DATA_MANAGER_H_
 
+#include "datatype_basic.h"
+#include "covisible_graph.h"
+
+#include "memory"
+
+namespace VIO {
+
+/* Class Data Manager Declaration. */
+class DataManager final {
+
+public:
+    using CovisibleGraphType = SLAM_UTILITY::CovisibleGraph<Vec3, Vec2>;
+
+public:
+    DataManager() = default;
+    ~DataManager() = default;
+
+    // Reference for member varibles.
+    CovisibleGraphType *local_map() { return local_map_.get(); }
+
+private:
+    std::unique_ptr<CovisibleGraphType> local_map_ = std::make_unique<CovisibleGraphType>();
+
+};
+
+}
+
 #endif // end of _VIO_STEREO_ORB_SLAM3_DATA_MANAGER_H_
