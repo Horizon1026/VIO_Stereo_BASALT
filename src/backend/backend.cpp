e40bdce6fafaf922ac6167e5b9206dc71477dca7
Horizon1026 <lijiapeng_atlantis@163.com>
2023-10-29 00:39:06 +0800

    update code

--- src/backend/backend.cpp
+++ src/backend/backend.cpp
@@ -1,8 +1,6 @@
 #include "backend.h"
 #include "log_report.h"
 
-#include "geometry_epipolar.h"
-#include "relative_rotation.h"
 #include "gyro_bias_solver.h"
 
 namespace VIO {
@@ -20,7 +18,7 @@ bool Backend::RunOnce() {
             ReportInfo(GREEN "[Backend] Backend succeed to initialize." RESET_COLOR);
         } else {
             ReportWarn("[Backend] Backend failed to initialize.");
-            return false;
+            return true;
         }
     }
 
@@ -82,6 +80,7 @@ bool Backend::EstimateGyroBiasForInitialization() {
     // Iterate all frames.
     const uint32_t max_frames_idx = data_manager_->visual_local_map()->frames().back().id();
     const uint32_t min_frames_idx = data_manager_->visual_local_map()->frames().front().id();
+    auto new_frame_iter = std::next(data_manager_->new_frames().begin());
     std::vector<FeatureType *> covisible_features;
     std::vector<Vec2> ref_norm_xy;
     std::vector<Vec2> cur_norm_xy;
@@ -98,31 +97,15 @@ bool Backend::EstimateGyroBiasForInitialization() {
         }
         ReportDebug("[Backend] Frame " << i << " and " << i + 1 << " have " << covisible_features.size() << " covisible features.");
 
-        // Debug: Estimate rotation and translation by 5pts methods.
-        // Mat3 essential = Mat3::Zero();
-        // Mat3 R_cr = Mat3::Identity();
-        // Vec3 t_cr = Vec3::Zero();
-        // std::vector<uint8_t> status;
-
-        // using namespace VISION_GEOMETRY;
-        // EpipolarSolver solver;
-        // solver.options().kMethod = EpipolarSolver::EpipolarMethod::kRansac;
-        // solver.options().kModel = EpipolarSolver::EpipolarModel::kFivePoints;
-        // solver.EstimateEssential(ref_norm_xy, cur_norm_xy, essential, status);
-        // solver.RecoverPoseFromEssential(ref_norm_xy, cur_norm_xy, essential, R_cr, t_cr);
-        // ReportDebug("R_cr is\n" << R_cr);
-        // ReportDebug("t_cr is " << t_cr.transpose());
-
-        // Debug: Estimate pure rotation by eigen solver.
-        // using namespace VISION_GEOMETRY;
-        // RelativeRotation solver;
-        // Quat q_cr = Quat::Identity();
-        // solver.EstimateRotationByBnb(ref_norm_xy, cur_norm_xy, q_cr);
-        // ReportDebug("R_cr is\n" << q_cr.matrix());
-
         // Precompute summation terms for iteration.
         SummationTerms terms;
         RelativeRotation::ComputeSummationTerms(ref_norm_xy, cur_norm_xy, terms);
+
+        // Localize the frame with bias in 'new_frames_' between frame i and i + 1.
+        ImuPreintegrateBlock &imu_preint_block = new_frame_iter->imu_preint_block;
+        ++new_frame_iter;
+        const Mat3 dr_dbg = imu_preint_block.dr_dbg();
+        ReportDebug("[Backend] Imu preintegrate dr_dbg is\n" << dr_dbg);
 
         // Estimate gyro bias by iteration.
         // Prepare for optimizaiton.
@@ -131,7 +114,6 @@ bool Backend::EstimateGyroBiasForInitialization() {
         x = Vec3::Zero();
 
         // Use LM solver in eigen3 to optimize x.
-        Mat3 dr_dbg = Mat3::Identity();
         GyroBiasSolveStep functor(terms, dr_dbg);
         Eigen::NumericalDiff<GyroBiasSolveStep> num_diff(functor);
         Eigen::LevenbergMarquardt<Eigen::NumericalDiff<GyroBiasSolveStep>, float> solver(num_diff);
@@ -142,9 +124,9 @@ bool Backend::EstimateGyroBiasForInitialization() {
         solver.minimize(x);
 
         // Recovery rotation matrix from cayley.
-        Vec3 cayley = x;
-        const Mat3 R_cr = Utility::ConvertCayleyToRotationMatrix(cayley);
-        q_cr = Quat(R_cr);
+        const Vec3 bias_g = x;
+        imu_preint_block.bias_gyro() = bias_g;
+        ReportDebug("[Backend] Estimated bias_g is " << LogVec(bias_g));
 
     }
 
--- src/backend/gyro_bias_solver.h
+++ src/backend/gyro_bias_solver.h
@@ -2,9 +2,15 @@
 #define _GYRO_BIAS_SOLVER_H_
 
 #include "eigen_optimization_functor.h"
+#include "eigen3/unsupported/Eigen/NonLinearOptimization"
+#include "eigen3/unsupported/Eigen/NumericalDiff"
+
 #include "relative_rotation.h"
+#include "math_kinematics.h"
 
 namespace VIO {
+
+using namespace VISION_GEOMETRY;
 
 /* Gyro Bias Solver Step Definition. */
 struct GyroBiasSolveStep : OptimizationFunctor<float> {
@@ -16,14 +22,17 @@ struct GyroBiasSolveStep : OptimizationFunctor<float> {
         terms_(terms), dr_dbg_(dr_dbg) {}
 
     int operator()(const Vec &x, Vec &fvec) const {
-        Vec3 cayley = x;
-        Mat1x3 jacobian = Mat1x3::Zero();
+        const Vec3 delta_bg = x;
+        const Vec3 delta_rot = dr_dbg_ * delta_bg;
+        const Quat q = Utility::Exponent(delta_rot);
+        const Vec3 cayley = Utility::ConvertRotationMatrixToCayley(q.matrix());
+        Mat1x3 dlambda_dcayley = Mat1x3::Zero();
+        RelativeRotation::ComputeSmallestEigenValueAndJacobian(terms_, cayley, dlambda_dcayley);
 
-        RelativeRotation::ComputeSmallestEigenValueAndJacobian(terms_, cayley, jacobian);
-
-        fvec[0] = jacobian(0, 0);
-        fvec[1] = jacobian(0, 1);
-        fvec[2] = jacobian(0, 2);
+        Mat1x3 dlambda_dbg = dlambda_dcayley * dr_dbg_;
+        fvec[0] = dlambda_dbg(0, 0);
+        fvec[1] = dlambda_dbg(0, 1);
+        fvec[2] = dlambda_dbg(0, 2);
 
         return 0;
     }
