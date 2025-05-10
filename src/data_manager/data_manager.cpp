de78a01315142da6e7f5f66f8db440f201866bef
Horizon1026 <lijiapeng_atlantis@163.com>
2025-05-10 21:36:16 +0800

    update code

--- src/data_manager/data_manager.cpp
+++ src/data_manager/data_manager.cpp
@@ -196,19 +196,12 @@ bool DataManager::SelfCheckVisualLocalMap() {
 
     // Iterate each frame to check all features.
     for (const auto &frame : visual_local_map_->frames()) {
-        const auto frame_id = frame.id();
         for (const auto &pair : frame.features()) {
             const auto feature_id = pair.first;
             const auto feature_ptr = pair.second;
             if (feature_id != feature_ptr->id()) {
                 ReportError("[DataManager] Visual local map self check frames, feature id error [" <<
                     feature_id << "] != [" << feature_ptr->id() << "].");
-                return false;
-            }
-
-            if (feature_ptr->observe(frame_id).front().frame_time_stamp_s != frame.time_stamp_s()) {
-                ReportError("[DataManager] Visual local map self check frames, feature observe timestamp error [" <<
-                    feature_ptr->observe(frame_id).front().frame_time_stamp_s << "] != [" << frame.time_stamp_s() << "].");
                 return false;
             }
         }
@@ -223,17 +216,6 @@ bool DataManager::SelfCheckVisualLocalMap() {
                 feature_id << "] != [" << feature.id() << "].");
             return false;
         }
-
-
-        for (auto frame_id = feature.first_frame_id(); frame_id <= feature.final_frame_id(); ++frame_id) {
-            const auto time_stamp_from_feature = feature.observe(frame_id).front().frame_time_stamp_s;
-            const auto time_stamp_from_frame = visual_local_map_->frame(frame_id)->time_stamp_s();
-            if (time_stamp_from_feature != time_stamp_from_frame) {
-                ReportError("[DataManager] Visual local map self check features, feature observe timestamp error [" <<
-                    time_stamp_from_feature << "] != [" << time_stamp_from_frame << "].");
-                return false;
-            }
-        }
     }
 
     ReportInfo("[DataManager] Visual local map self check ok.");
@@ -243,19 +225,6 @@ bool DataManager::SelfCheckVisualLocalMap() {
 bool DataManager::SelfCheckFramesWithBias() {
     // Iterate each frame with bias.
     for (const auto &frame_with_bias : frames_with_bias_) {
-        // Check timestamp of visual observations.
-        for (const auto &observes : frame_with_bias.visual_measure->observes_per_frame) {
-            const auto time_stamp_0 = frame_with_bias.time_stamp_s;
-            for (const auto &observe : observes) {
-                const auto time_stamp_1 = observe.frame_time_stamp_s;
-                if (time_stamp_0 != time_stamp_1) {
-                    ReportError("[DataManager] Frames with bias self check frontend output data, feature observe timestamp error [" <<
-                        time_stamp_0 << "] != [" << time_stamp_1 << "].");
-                    return false;
-                }
-            }
-        }
-
         // Check timestamp of imu and images.
         const auto latest_imu_time_stamp_s = frame_with_bias.packed_measure->imus.back()->time_stamp_s;
         const auto oldest_imu_time_stamp_s = frame_with_bias.packed_measure->imus.front()->time_stamp_s;
