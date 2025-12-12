19353885e6400464b3cfd9536146825a1875e6d5
Horizon1026 <lijiapeng_atlantis@163.com>
2025-12-13 00:00:04 +0800

    update code

--- src/vio_config.cpp
+++ src/vio_config.cpp
@@ -1,14 +1,11 @@
 #include "vio.h"
-
-#include "pinhole.h"
-
-#include "optical_flow_basic_klt.h"
-
-#include "feature_point_detector.h"
-#include "feature_point_fast_detector.h"
-
 #include "slam_log_reporter.h"
 #include "slam_operations.h"
+
+#include "camera_model.h"
+#include "optical_flow_basic_klt.h"
+#include "feature_point_detector.h"
+#include "feature_point_fast_detector.h"
 
 namespace vio {
 
@@ -86,7 +83,7 @@ bool Vio::ConfigComponentOfDataLoader() {
 }
 
 bool Vio::ConfigComponentOfFrontend() {
-    using CameraType = sensor_model::Pinhole;
+    using CameraType = sensor_model::CameraPinholeRadtan;
     using FeatureType = feature_detector::FeaturePointFastDetector;
     using KltType = feature_tracker::OpticalFlowBasicKlt;
 
